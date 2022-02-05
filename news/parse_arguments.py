b40093f044c692107c5f0ea0e2f11b86f85fd009
Tarik Souabny <tsouabny@bostonk12.org>
2022-02-05 13:01:13 +0000

    Added optional Requirements to parse_arguments.py

--- news/parse_arguments.py
+++ news/parse_arguments.py
@@ -2,8 +2,8 @@ import argparse
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument(dest="type", help="Is the request a search or a retreival for the top headlines?")
-    parser.add_argument(dest="search_keyword", help="Search keyword(s). If retreiving top headlines, put in 0")
-
+    parser.add_argument(dest="type", help="Is the request a search or a retreival for the top headlines?", type=str)
+    parser.add_argument(dest="search_keyword", help="Search keyword(s), if possible",nargs="?" , default="")
     arguments = parser.parse_args()
     return arguments
+print(parse_arguments())
