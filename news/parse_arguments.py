da3989cfb198ede5da741dd482069b3cf98f826e
Tarik Souabny <tsouabny@bostonk12.org>
2022-02-06 13:08:27 +0000

    Added a function to print help text

--- news/parse_arguments.py
+++ news/parse_arguments.py
@@ -9,6 +9,7 @@ def parse_arguments():
     parser.add_argument(dest="type_", help="Is the request a search or a retreival for the top headlines?", type=str)
     parser.add_argument(dest="search_keyword", help="Search keyword(s), if possible",nargs="?" , default="")
     #add a help text argument
+    parser.add_argument(dest="help_", help="Does the user need the help info?", nargs="?", default="")
     arguments = parser.parse_args()
     logging.debug(str(arguments))
     are_arguments_valid(arguments)
