20b5ae54bf56c8c21c124032ac7ebccb45aa83b4
Tarik Souabny <tsouabny@bostonk12.org>
2022-02-06 12:22:00 +0000

    Updated type variable name to type_

--- news/parse_arguments.py
+++ news/parse_arguments.py
@@ -1,8 +1,14 @@
 import argparse
+import util
+import logging
 
+@util.log_start_end_func
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument(dest="type", help="Is the request a search or a retreival for the top headlines?", type=str)
+    parser.add_argument(dest="type_", help="Is the request a search or a retreival for the top headlines?", type=str)
     parser.add_argument(dest="search_keyword", help="Search keyword(s), if possible",nargs="?" , default="")
     arguments = parser.parse_args()
+    logging.debug(str(arguments))
     return arguments
+
+parse_arguments()
