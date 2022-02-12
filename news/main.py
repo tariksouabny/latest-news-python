77247606f675759e40ee894489d72e6a20fe3d0b
tariksouabny <tsouabny@bostonk12.org>
2022-02-12 13:11:09 +0000

    Added TODOs

--- news/main.py
+++ news/main.py
@@ -1,6 +1,7 @@
 import check_arguments, parse_arguments, util
 import logging, pprint
-
+#TODO: define a way to get a token from check_arguments that defines whether or not to execute this file
+#TODO: define a way to get a token from check_arguments that defines whether to search or give a feed
 util.inherit_logging_config()
 
 import requests, json
@@ -19,3 +20,5 @@ response = requests.request("GET", url, headers=headers, params=querystring)
 response_dict = json.loads(response.text)
 for article in response_dict['articles']:
     print(util.format_search_article(article))
+
+#TODO: Find a way to access a feed
