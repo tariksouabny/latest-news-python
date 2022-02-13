1586cd8354416b09ba4e53fe816174a60a67395e
tariksouabny <tsouabny@bostonk12.org>
2022-02-13 12:27:20 +0000

    Finished search func; added script functionality to main.py

--- news/main.py
+++ news/main.py
@@ -6,23 +6,24 @@ util.inherit_logging_config()
 
 import requests, json
 
-url = "https://free-news.p.rapidapi.com/v1/search"
+URL = "https://free-news.p.rapidapi.com/v1/search"
 
-querystring = {"q":"Jazz","lang":"en"}
+#querystring = {"q":"Jazz","lang":"en"}
 
-headers = {
+HEADERS = {
     'x-rapidapi-host': "free-news.p.rapidapi.com",
     'x-rapidapi-key': "f67acd1fbamsh537007e203f8ab5p12c2b5jsna64615480c4b"
     }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
-
-response_dict = json.loads(response.text)
-for article in response_dict['articles']:
-    print(util.format_search_article(article))
+#response = requests.request("GET", url, headers=headers, params=querystring)
 
 def search_func(search_keyword):
-    pass
+    querystring = {"q": f"{search_keyword.lower()}", "lang": "en"}
+    response = requests.get("GET", URL, headers=HEADERS, params=querystring)
+    response_dict = json.loads(response.text)
+    for article in response_dict['articles']:
+        print(util.format_search_article(article))
+
 
 def feed_func():
     pass
@@ -30,15 +31,16 @@ def feed_func():
 def main():
     arguments = parse_arguments.parse_arguments()
     valid, argument_type_, argument_search_keyword = check_arguments.are_arguments_valid(arguments)
-    if valid():
+    if valid:
         dict_lookup = {
-            'search': lambda x: search_func(x, argument_search_keyword),
+            'search': lambda x: search_func(argument_search_keyword),
             'feed': feed_func,
 
         }
         logging.debug(dict_lookup[argument_type_])
-        dict_lookup[argument_type_]()
+        dict_lookup[argument_type_](...)
         
-
+if __name__ == "__main__":
+    main()
 
 #TODO: Find a way to access a feed
