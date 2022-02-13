b7d472777c90b95209c8a514e339d2f56e49a0e7
tariksouabny <tsouabny@bostonk12.org>
2022-02-13 12:33:51 +0000

    Updated logging in util.py - resolving issue in main.py

--- news/util.py
+++ news/util.py
@@ -5,23 +5,14 @@ import logging
 def inherit_logging_config():
     logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 
-def log_start_end_func(func):
-    inherit_logging_config()
-    def log(*args, **kwargs):
-
-        #TODO: Add a way to log the **kwargs
-        logging.debug(f"Start of {func.__name__}{args}")
-        return_val = func(*args, **kwargs) #if kwargs is not None else func(*args)
-        logging.debug(f"End of {func.__name__}{args}")
-    return log
 
 def log_start_end_func(func):
     inherit_logging_config()
     def log(*args, **kwargs):
         # TODO: Add a way to log **kwargs as well
-        logging.debug(f"Start of {func}{args}")
+        logging.debug(f"Start of {func.__name__}{args}")
         return_val = func(*args, **kwargs)
-        logging.debug(f"End of {func}{args}")
+        logging.debug(f"End of {func.__name__} returning {return_val}")
         return return_val
     return log
 @log_start_end_func
