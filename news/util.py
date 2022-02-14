1ee71539b99b7fbd149bcfb61bad86c2c3178bd8
tariksouabny <tsouabny@bostonk12.org>
2022-02-14 23:10:10 +0000

    Updated inner functions

--- news/util.py
+++ news/util.py
@@ -2,25 +2,14 @@ import logging
 #TODO: rename to util_news.py - std module already named after this flie
 
 def inherit_logging_config():
-    logging.basicConfig(level=logging.CRITICAL, format="%(asctime)s - %(levelname)s - %(message)s")
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 
-def log_start_end_func(func):
-    inherit_logging_config()
-    def log(*args, **kwargs):
-
-        #TODO: Add a way to log the **kwargs
-        logging.debug(f"Start of {func.__name__}{args}")
-        return_val = func(*args, **kwargs) #if kwargs is not None else func(*args)
-        logging.debug(f"End of {func.__name__}{args}")
-    return log
-
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 
 def log_start_end_func(func):
     def log(*args, **kwargs):
         # TODO: Add a way to log **kwargs as well
-        logging.debug(f"Start of {func}{args}")
+        logging.debug(f"Start of {func.__name__}{args}")
         return_val = func(*args, **kwargs)
-        logging.debug(f"End of {func}{args}")
+        logging.debug(f"End of {func.__name__}{args}")
         return return_val
     return log
